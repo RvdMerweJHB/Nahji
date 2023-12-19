6786e1f8c9c6afc8e038843d7dc2b435ed1ca134
RvdMerweJHB <rudolphvdmerwe123@gmail.com>
2023-12-19 19:45:13 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SolverModule } from './solver/solver.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    SolverModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
