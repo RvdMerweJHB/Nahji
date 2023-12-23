512be71f4436348466876a3d40b7100c6a82f8f1
RvdMerweJHB <rudolphvdmerwe123@gmail.com>
2023-12-23 22:18:36 +0200

    basic models add

--- src/app/solver/solver-wrapper/solver-tile/solver-tile.component.ts
+++ src/app/solver/solver-wrapper/solver-tile/solver-tile.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TileLocation } from '../../models/tile-location';
+import { Tile } from '../../models/tile';
 
 @Component({
   selector: 'app-solver-tile',
   templateUrl: './solver-tile.component.html',
   styleUrl: './solver-tile.component.scss'
 })
-export class SolverTileComponent {
-
+export class SolverTileComponent implements OnInit {
+    public Tile: Tile = new Tile();
+    
+    ngOnInit(): void {
+    }  
 }
--- src/app/solver/solver-wrapper/solver-wrapper.component.ts
+++ src/app/solver/solver-wrapper/solver-wrapper.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tile } from '../models/tile';
 
 @Component({
   selector: 'app-solver-wrapper',
   templateUrl: './solver-wrapper.component.html',
   styleUrl: './solver-wrapper.component.scss'
 })
-export class SolverWrapperComponent {
+export class SolverWrapperComponent implements OnInit {
+    public Tiles: Tile[][] | undefined;
+    public Rows: number = 3;
+    public Columns: number = 3;
+
+    ngOnInit(): void {
+        this.Tiles = [[new Tile(), new Tile(), new Tile()], [new Tile(), new Tile(), new Tile()], [new Tile(), new Tile(), new Tile()]];
+    }
 }
